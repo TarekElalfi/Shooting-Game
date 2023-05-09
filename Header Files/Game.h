b982e31b706dfa058b96e627e7024081bc099e64
Khaled Saleh <93226474+khaled-saleh1@users.noreply.github.com>
2023-05-09 05:32:13 -0400

    Started working class game. Can't link the library SFML to the CMakeLists.txt and which we call in Game.h file. What creates initWindow(window)

---
+++ Header Files/Game.h
@@ -0,0 +1,35 @@
+//
+// Created by khaled saleh on 2023-05-09.
+//
+
+#ifndef SHOOTING_GAME_GAME_H
+#define SHOOTING_GAME_GAME_H
+#include <SFML/Graphics.hpp>
+#include <include/SFML/Graphics.hpp>
+
+
+#include <tkDecls.h>
+
+
+class Game
+ {
+private:
+     sf:RenderWindow* window;
+     void initWindow();
+
+ public:
+     Game();
+     ssa
+
+ virtual ~Game();
+     // Functions
+     void run();
+     void update();
+     void render();
+
+ };
+
+
+
+
+#endif //SHOOTING_GAME_GAME_H
